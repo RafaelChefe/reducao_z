8aeac86a0aac1377bc4d95a4dda545923923e24a
Rafael Santos <estradeiro@gmail.com>
2013-04-24 21:54:06 -0300

    refactoring tests with a helper method

--- test_parser.rb
+++ test_parser.rb
@@ -85,53 +85,34 @@ class TestReductionZ < Test::Unit::TestCase
       QQQQQQQQQWYRREQQTU BR
       ------------------------------------------------
     EOS
-
-    @parser = Parser.new
   end
 
-  def test_parse
-    redz = {
-      :id => 17936,
-      :data_movimento => Date.parse("05/03/2013"),
-      :cont_reinicio_operacao => "004",
-      :cont_reducao_z => "1330",
-      :cont_operacoes_nao_fiscais => "005185",
-      :cont_comp_deb_cred => "0004"
-    }
+  def helper(text, redz)
+    assert_equal redz, Parser.new.parse(text)
+  end
 
-    reducao_z = @parser.parse(@text)
+  def test_id
+    helper("05/03/2013 19:09:14 COO:017936\n", { :id => 17936 })
+  end
 
-    assert_equal redz, reducao_z
+  def test_data_movimento
+    helper("MOVIMENTO DO DIA: 05/03/2013\n", { :data_movimento => Date.parse("05/03/2013") })
+  end
+
+  def test_cont_reducao_z
+    helper("Contador de Reduções Z: 1330", { :cont_reducao_z => "1330" })
   end
 
   def test_cont_reinicio_operacao
-    text = "Contador de Reinício de Operação: 004\n"
-
-    redz = { :cont_reinicio_operacao => "004" }
-
-    reducao_z = @parser.parse(text)
-
-    assert_equal redz, reducao_z
+    helper("Contador de Reinício de Operação: 004\n", { :cont_reinicio_operacao => "004" })
   end
 
   def test_cont_operacoes_nao_fiscais
-    text = "Geral de Operação Não Fiscal: 005185"
-
-    redz = { :cont_operacoes_nao_fiscais => "005185" }
-
-    reducao_z = @parser.parse(text)
-
-    assert_equal redz, reducao_z
+    helper("Geral de Operação Não Fiscal: 005185\n", { :cont_operacoes_nao_fiscais => "005185" })
   end
 
   def test_cont_comp_deb_cred
-    text = "Comprovante de Crédito ou Débito: 0004"
-
-    redz = { :cont_comp_deb_cred => "0004" }
-
-    reducao_z = @parser.parse(text)
-
-    assert_equal redz, reducao_z 
+    helper("Comprovante de Crédito ou Débito: 0004\n", { :cont_comp_deb_cred => "0004" })
   end
 
 end
